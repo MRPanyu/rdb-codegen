4e685a930336150a3dd0226c673db6b84aaf69fd
Panyu <mrpanyu@aliyun.com>
2017-07-25 12:26:53 +0800

    add MySqlJDBCModelReader, some bug fix

--- src/main/java/rdb/codegen/reader/MySqlJDBCModelReader.java
+++ src/main/java/rdb/codegen/reader/MySqlJDBCModelReader.java
@@ -15,7 +15,7 @@ import rdb.codegen.framework.model.Table;
 import rdb.codegen.framework.model.Unique;
 import rdb.codegen.framework.utils.Utils;
 
-public class OracleJDBCModelReader extends AbstractJDBCModelReader {
+public class MySqlJDBCModelReader extends AbstractJDBCModelReader {
 
 	@Override
 	public Database read() throws Exception {
@@ -23,9 +23,6 @@ public class OracleJDBCModelReader extends AbstractJDBCModelReader {
 		Connection conn = getConnection();
 		try {
 			readTableNames(conn, model);
-			for (Table table : model.getTables()) {
-				readTableComment(conn, table);
-			}
 			for (Table table : model.getTables()) {
 				readTableColumns(conn, table);
 			}
@@ -41,14 +38,15 @@ public class OracleJDBCModelReader extends AbstractJDBCModelReader {
 	private void readTableNames(Connection conn, Database model) throws Exception {
 		String[] array = getTableNamesAsArray();
 		for (String tableNameLike : array) {
-			PreparedStatement ps = conn
-					.prepareStatement("select table_name from user_tables where upper(table_name) like ?");
-			ps.setString(1, tableNameLike.toUpperCase());
+			PreparedStatement ps = conn.prepareStatement(
+					"select table_name, table_comment from information_schema.tables where table_schema=? and upper(table_name) like ?;");
+			ps.setString(1, getSchema());
+			ps.setString(2, tableNameLike.toUpperCase());
 			ResultSet rs = ps.executeQuery();
 			while (rs.next()) {
-				String tableName = rs.getString(1);
 				Table table = new Table();
-				table.setTableName(tableName);
+				table.setTableName(rs.getString(1));
+				table.setComment(rs.getString(2));
 				model.getTables().add(table);
 			}
 			rs.close();
@@ -56,21 +54,11 @@ public class OracleJDBCModelReader extends AbstractJDBCModelReader {
 		}
 	}
 
-	private void readTableComment(Connection conn, Table table) throws Exception {
-		PreparedStatement ps = conn.prepareStatement("select comments from user_tab_comments where table_name=?");
-		ps.setString(1, table.getTableName());
-		ResultSet rs = ps.executeQuery();
-		if (rs.next()) {
-			table.setComment(rs.getString(1));
-		}
-		rs.close();
-		ps.close();
-	}
-
 	private void readTableColumns(Connection conn, Table table) throws Exception {
 		PreparedStatement ps = conn.prepareStatement(
-				"select column_name, data_type, data_length, data_precision, data_scale, nullable, data_default, column_id from user_tab_columns where table_name=? order by column_id asc");
-		ps.setString(1, table.getTableName());
+				"select column_name, data_type, character_maximum_length, numeric_precision, numeric_scale, is_nullable, column_default, column_comment, ordinal_position from information_schema.COLUMNS where table_schema=? and table_name=? order by ordinal_position");
+		ps.setString(1, getSchema());
+		ps.setString(2, table.getTableName());
 		ResultSet rs = ps.executeQuery();
 		while (rs.next()) {
 			Column column = new Column();
@@ -79,26 +67,15 @@ public class OracleJDBCModelReader extends AbstractJDBCModelReader {
 			column.setLength(rs.getInt(3));
 			column.setPrecision(rs.getInt(4));
 			column.setScale(rs.getInt(5));
-			column.setNullable("Y".equalsIgnoreCase(rs.getString(6)));
+			column.setNullable("YES".equalsIgnoreCase(rs.getString(6)));
 			column.setDefaultValue(rs.getString(7));
+			column.setComment(rs.getString(8));
 			table.getColumns().add(column);
 		}
 		rs.close();
 		ps.close();
-		// get column comments
-		ps = conn.prepareStatement("select comments from user_col_comments where table_name=? and column_name=?");
-		for (Column column : table.getColumns()) {
-			ps.setString(1, table.getTableName());
-			ps.setString(2, column.getColumnName());
-			rs = ps.executeQuery();
-			if (rs.next()) {
-				column.setComment(rs.getString(1));
-			}
-			rs.close();
-		}
-		ps.close();
 		// get column meta data
-		ps = conn.prepareStatement("select * from " + table.getTableName() + " where 1=0");
+		ps = conn.prepareStatement("select * from " + getSchema() + "." + table.getTableName() + " where 1=0");
 		rs = ps.executeQuery();
 		ResultSetMetaData meta = rs.getMetaData();
 		for (int i = 1; i <= meta.getColumnCount(); i++) {
@@ -115,29 +92,28 @@ public class OracleJDBCModelReader extends AbstractJDBCModelReader {
 	}
 
 	private void readTableConstraints(Connection conn, Table table) throws Exception {
-		Map<String, String> foreignKeyRefConstraints = new HashMap<String, String>();
 		PreparedStatement ps = conn.prepareStatement(
-				"select constraint_name, constraint_type, r_constraint_name, index_name from user_constraints where table_name=? and constraint_type in ('P', 'U', 'R')");
-		ps.setString(1, table.getTableName());
+				"select constraint_name, constraint_type from information_schema.table_constraints where table_schema=? and table_name=?");
+		ps.setString(1, getSchema());
+		ps.setString(2, table.getTableName());
 		ResultSet rs = ps.executeQuery();
 		while (rs.next()) {
 			String type = rs.getString(2);
-			if ("P".equals(type)) {
+			if ("PRIMARY KEY".equalsIgnoreCase(type)) {
 				PrimaryKey primaryKey = new PrimaryKey();
 				primaryKey.setName(rs.getString(1));
-				primaryKey.setIndexName(rs.getString(4));
+				primaryKey.setIndexName(primaryKey.getName());
 				table.setPrimaryKey(primaryKey);
-			} else if ("U".equals(type)) {
+			} else if ("UNIQUE".equalsIgnoreCase(type)) {
 				Unique unique = new Unique();
 				unique.setName(rs.getString(1));
-				unique.setIndexName(rs.getString(4));
+				unique.setIndexName(unique.getName());
 				table.getUniques().add(unique);
-			} else {
+			} else if ("FOREIGN KEY".equalsIgnoreCase(type)) {
 				ForeignKey foreignKey = new ForeignKey();
 				foreignKey.setName(rs.getString(1));
-				foreignKey.setIndexName(rs.getString(4));
+				foreignKey.setIndexName(foreignKey.getName());
 				table.getForeignKeys().add(foreignKey);
-				foreignKeyRefConstraints.put(foreignKey.getName(), rs.getString(3));
 			}
 		}
 		rs.close();
@@ -147,11 +123,13 @@ public class OracleJDBCModelReader extends AbstractJDBCModelReader {
 		for (Column column : table.getColumns()) {
 			columnMap.put(column.getColumnName(), column);
 		}
-		ps = conn.prepareStatement(
-				"select column_name, position from user_cons_columns where constraint_name=? order by position asc");
+		String sql = "select column_name, referenced_table_name, referenced_column_name, ordinal_position from information_schema.key_column_usage where table_schema=? and table_name=? and constraint_name=? order by ordinal_position";
 		PrimaryKey primaryKey = table.getPrimaryKey();
 		if (primaryKey != null) {
-			ps.setString(1, primaryKey.getName());
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, getSchema());
+			ps.setString(2, table.getTableName());
+			ps.setString(3, primaryKey.getName());
 			rs = ps.executeQuery();
 			while (rs.next()) {
 				String columnName = rs.getString(1);
@@ -161,7 +139,10 @@ public class OracleJDBCModelReader extends AbstractJDBCModelReader {
 			rs.close();
 		}
 		for (Unique unique : table.getUniques()) {
-			ps.setString(1, unique.getName());
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, getSchema());
+			ps.setString(2, table.getTableName());
+			ps.setString(3, unique.getName());
 			rs = ps.executeQuery();
 			while (rs.next()) {
 				String columnName = rs.getString(1);
@@ -171,39 +152,21 @@ public class OracleJDBCModelReader extends AbstractJDBCModelReader {
 			rs.close();
 		}
 		for (ForeignKey foreignKey : table.getForeignKeys()) {
-			ps.setString(1, foreignKey.getName());
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, getSchema());
+			ps.setString(2, table.getTableName());
+			ps.setString(3, foreignKey.getName());
 			rs = ps.executeQuery();
 			while (rs.next()) {
 				String columnName = rs.getString(1);
 				foreignKey.getColumnNames().add(columnName);
 				columnMap.get(columnName).setForeignKey(true);
+				foreignKey.setReferenceTableName(rs.getString(2));
+				foreignKey.getReferenceColumnNames().add(rs.getString(3));
 			}
 			rs.close();
 		}
 		ps.close();
-		// get foreign key reference table/columns
-		ps = conn.prepareStatement("select table_name from user_constraints where constraint_name=?");
-		for (ForeignKey foreignKey : table.getForeignKeys()) {
-			String refConstraintName = foreignKeyRefConstraints.get(foreignKey.getName());
-			ps.setString(1, refConstraintName);
-			rs = ps.executeQuery();
-			if (rs.next()) {
-				foreignKey.setReferenceTableName(rs.getString(1));
-			}
-			rs.close();
-		}
-		ps.close();
-		ps = conn.prepareStatement(
-				"select column_name, position from user_cons_columns where constraint_name=? order by position asc");
-		for (ForeignKey foreignKey : table.getForeignKeys()) {
-			String refConstraintName = foreignKeyRefConstraints.get(foreignKey.getName());
-			ps.setString(1, refConstraintName);
-			rs = ps.executeQuery();
-			while (rs.next()) {
-				foreignKey.getReferenceColumnNames().add(rs.getString(1));
-			}
-			rs.close();
-		}
 	}
 
 }
